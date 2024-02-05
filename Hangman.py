222743c46065d04b5926cc9732f58c5d39f8f1a0
Michelle Simon <158987524+mlyysimon@users.noreply.github.com>
2024-02-04 21:58:45 -0800

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,12 +1,13 @@
 import random
-from words import words
+from words import words # dataset of words in hangman
 import string
 
 def get_valid_word(words):
     word = random.choice(words)
+    # make sure words don't have a dash or space
     while '-' in word or ' ' in word:
         word = random.choice(words)
-        
+
     return word.upper()
 
 def hangman():
@@ -14,17 +15,20 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-    
+
     lives = 6
     
     while len(word_letters) > 0 and lives > 0:
+        # reveal which letters have been guessed
         print(f"\nYou have {lives} lives left and you have used these letters: ", ' '.join(used_letters))
-                
+
+        # reveal current known letters within the word
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
         
         user_letter = input('\nGuess a letter: ').upper()
         if user_letter in alphabet - used_letters:
+            # tracks which letters have been guessed
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
